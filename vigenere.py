318c0adefdce632828fd64533c2dc81c3eabfd44
efilippini <36900253+efilippini@users.noreply.github.com>
2023-04-20 09:41:02 -0300

    Create vigenere.py

---
+++ vigenere.py
@@ -0,0 +1,92 @@
+# Texto a ser decifrado.
+
+CIPHER_TEXT = "cpimvtcmngiheycgremhwltmvtceiiqwivwhmumhwptawgoptfizftgitlezpcgxfspsbuchhbqwhalhdsaosxyceispdtbxoetzwtsmcgjoyoiseggpdwxzalstddwimwrlltbsahrgxnaedwtlhgdahrhwpfkovxeghdqtasrcimwpdhtgvtvxbqtrmvizapcgwdyobzultxntbccdevccoogzntnbaezrmocnemcisemoapoyhgzytbsehxztregrdqakhwfrtbstsloxoaloulcmhdsaosxysiwgpdlcbpoyhwpetfajntjxramcgdoyhwpsblipeghwneghjcymvxdmrhwtctzillxcuhhbqwehxgjmjxqihalowtsmcgjoyhwpwtfhzfmvtltasctaggprabbhetasxdltbszfthalnmwhtslieaolsseousuzugrtouicclnnbutnbgwpdictxoygdwoghdhhbqwttpcjwdaokpsmcdoighwpstatceeoitogohehxkgttbbvdoyhwplhudrrtdwprlhdehxddpmlcusofsgttpcjwdaokpthzszftgicuzuapfhfatbxfijcihxxightydxrizrxdgpsxbiehxqdyfewreoydtcsbopydasawalktxarxjogxtgzmmvtyouztnofatycxatythtisemwblengucofhwpfkovxeghdqtasrcimwpdimgtwftbsqrhaisemvxcducdvoyhwpltkhtnpvpemtbcprizpeopcjwdaokptkspeewhwtsawvsakujxeghlpctbdylrujpslkwjtasvcethspsbuchaloqlnwccpdisgsaigqpctihppeoizbxqpxelscdiuztzflcbpigqdygkixeybbpqivhxeihihsilhdcyhfqpctihphxvpolhgisilwceeksheigwizrusrlulspovtbrtnzmtlrltdcbtrtehxqdxpesitogcutttbshefonalxohponfhploshhimviseyocnymvpehtrisilwblgbbpcygogcamwkpeosgmexbutnbgwpdpshsonzssaosuzugrewamcwtmlsaqsraeltawhtnzkxehmvtdtkivrlxtdchxzapnbqxydxdtydxbrpcizphslwcrigupsyfbdqtkwjxpackprfogltacclnwgpwafwhaekvpasfoztnzhwprxtapcmwdyoyvtcowcifspvtceasrznmsbalthtdtasvcophwzfmvtltasctagsbaikswzwufpgethwtnzwhqrxsszmhthaexqwhhbqwsalapoemvtltasctagghzftfticxsspvxfnztasgdtthtzfasawalwcrrxoiyelgdcmhftarhppmlroierbpjeiguiseowreokmiztaspycbsceghcszrwsgzfthwpnlocothhwpftjdcoyoezlecpydthwpnxqetnmfdothqgttbohlgtwcalthdxarptcezogoewohehxqpattwclraqwpghndclxosprhtprohrajbtbszfycawopsgdfhfxytasgppnpatcbgizbxtdfnwhwpokwvtntzdqcbqtcolrtceiiqwivodqsmojrulhxyelqxeyhtvzdhtisenhdaitcudikhwzmtgbzrxocooyhwpnnatcongdehxfxxazwclrrgiltxglsivvpceyfpxewieznmvtdafsbzdxzisexlipnmhdhhbqwlrbgiztesdctaspcilhdeeewpysvvdzlpsgpigrtmtxrizhbaxytasezlbhxnsaohmexbattmztcevcvyilsslnwhwprxqdrnbhxznbgisefcgpnxqtdstfnmevojdebhxdnhhbldxpnlrbgizteswtmlsaqtasihoivxwolcesekgwldfcgpigqdxmhbisaghwpypsgpchbhnihihzftbsarhppmlrgdxexztxeghhzfizpeoksbliggitleicoemsreewwclrbgiztesxyeguatsadwtlhgdahrhdzmtbnlfywcttbshxarptertqtonhhdylrwcehxkdcklcuehxqpxbkwsreizpeogwhesuiitnzftlthfxrigoahrbhtcsewzpbxfzplxmdcchztciwuteoizpeotbssilwspalhwltmvtcebgpernhwsizvtctaocpxisgtegqtzfpvxnhmvtxigrqpakglttgshdthvtcsxzutstqdyvbqitogkwtcawczukclygxbtcamwdyhtgqpegscehngxlsmwrllempdsxfipdtbstsisgsaigvligwcrgkcjydhtisezftpktiisokglsothiseksclilgpycxpgzuzvilnxkatfxwceomvthokzsalthdsalvpotasvcethtdtbbuwuxbrptasgppnpatchtewamcxdaegdehxtxcsmhgpamwhpuiccpdnqpeihbdqwawrstaslcimwcrshtbtlmcclnwzdnkxfdfslspfjxocaanzpydzctehxogptasapgbhxxamsspsvscoaghhwidsslnmsdcbnbnlnaswlstftgeeoitogculnhhwprewuplbytmavccsebgecoycjydemxxpkshdewkxehmvtfnbhnzfdbdhlxrvpighwpetfajcaignhastiekqxdewogpaewcqlnscnehbisehzdrytbsltmvtceowkllhtattxfpeuksdyphzxeivgtgeghwpfkovxeghhzfawhhokrhhhxbgppxoipdthhpchbssagrhjmivpgebbpwltutdrtjxdhxriseaspctlcuxegkwzhtjtdexbgpfesreewwcehxaisebfdhnawvsekbpeukswpilhwpfthwprhtxoetzxdmbbesiechzpamxyphzxeivgxylbhtcamigpagrblnrcuehxzpeelhrznvseeihbhzffcsprghwtndsgdagrheamshxeggjnhtgisenbxeyhtzyopztogxhwprxwvyoyzphagrisexejllbhnzfmvtdeqshsaosqpegoceivweltxrxyawftlmumwtmmvtlrzibpnmcuehxftauuzxnilhwpsxognhttiprciheivsisegoifrxcuhhbqwtsywgdtawceewoimyvsesaeihehxxjdttbsmltatwelgdwdfocehxbstsvihdewccehxppdilcuarhjtcbboaxokoattrpndovfpeelocophztxakqwfsmvtyctfxnamigpdumisrtgnxavvjdagrelrmwpwlrsmaltwcpdumhzckoipskssfcxrizagoqdtkoreihbqjgeojnogocoawsxxaghjdagrwlvbbvmevcbpigjxdiuzttnmvttnwwktdnoacetdepakgpelxbvehbbisebrtlllhpeepvxnhbgrznlhgfcmssmylcrcamshehxtxcsmqpcehtisekiaprlwheoustouvoitogcuhhbqwlnhiiwigsxddkolyayhtctasdwdasawegwrxowsaarhjxoigudylrtdcagwbarhjtorxzxrihbpydfcgllbhnlnwadcelwbalbqxeybbbfsbqpydzmbyalhxnafocwixfhertwczficterrocogkspeekvpcmhbnzfmvttnwwktdnoalnwhwpsmoipwxogptaihwewcceomvtnogqtatbcczftvxrhxfheamsxywawrsnhapyctzadagmisiguwtshkclnwwchhbqwehxfttsgsxehxfblrkmxyggcgriowcrigapcrbovpagrztnzgpceivxwolcesekgpydivxwolcesekgpcedwcrstbsehxfttstbdehxfpydawvsekssfcthxznbbiplesreutzpdwxzalsfcglltbsceewvtongdqsvwtycxohheezpdoyogeagrczthtnzumvdylrpjeoyhwpwacapoyzxqelirsalhpeebgwlrwzneousgpaewopdbbisilkdclwocoqnwrvlrrtregsgltxgiztaseprysreiwspwsnqrpewgisezckprgatythtiselcaoixfpydmvtwoosgzfacczukhwtstuptnwsrwigwcrighdoefcrcavmpydwsbzckorjighdeykocyybbpyifovtntfnmumftrueogzrwsgsaowcrnhhbfcaftdefpalnvsiztaspntnoaqavhhhhxbisepvtplaohnofsufleqxcceslpdhbdebxuxyazoxywbhwlnxkeprbcszfaiblnewupbnhlphtjtaalgtofkcbehxptdtmcisepcgdttbsehxfthexbsehxgjmjxqitsmvtycaocrewocotasdwdjipcrxzdqphsicytbsahbzddoivnhhbqwsawptpnfcgplbuwelrhgpamsstnmvtpakzxprucdvshtiseksefbewrtsgclcelibpdtbsqonuweonhizavccnlngxznicterrwhoilqdgeksseouspyifwiltbccehkwrprxadgewtgzmmvternhwlnwvdxekohheezpdtasscafoitcictesaoktnzptpnvccoefbtoaloctmbhpeokwhdeghxythppyilvbpnmoaznzkxehmvtxagrisebrtloyhwpsmoipilgjapesbpnmssmymvtceosaltbcczfttjeuksatfxhwpdbjxdihbxythpdzklzxvetzadifwalrwwktsbccdcigxcgvzthilwcehxqalslwrllfihpufwharhppmlrzpeekhwlnmvtlgxcualthdehxbpeukoaoiowhtoggpceywkpigbjxbxfqzodwpydmvtqikgisaetdqbhcztiwclythhwpptfprrtdwmezwcyiguxsawoahargpombftotasvpnbihzfzzpfchbpydtrttmtbifspvxnhbgxytkcsfcmcgjtasutrlhqzodqdyttwctnzogpfnhpeihbdqtasezpnzpcagrhzpawheivoayomwdyshtyfsmwrpagrrznvzjoiguatkxgdxehtisexogwixfstaecvfelkxehhiilrkwktnzoilnrrtqigwiprxgjwtmcisilwhlpiscoewogpsmoipmxbizfmvtyamigpoyxjdtbqtlcvcgoiguizchabznhdxyihbpydtbpyspsgtswsblnwsseomvtbuxgitogkwltbgyfsmwrpsmfxapxrdqaidtlrtbrpsmvtdevccodbjxdihbxyceispsmvtcefoxydxfdqtashpchbslnwhwpwacapoyhwptawgoagruzukhwmohyhhhbqwlrxaptnemdncndxpdpwistasrznlhgfcmwdyoyhwpfbfhesmoipagriseywgdtxrjnamwdytasisikrstvbgxznvccdilhhzfmvtqiyhwdiqhwlnwgtgeghwmohyhtnpvxnhivxwolcesykoisekhwlnciheivsxdtashfbcsreoyscbubfnlnwhwpsxqdydlhpeebgrznlhgfcmssznifxycbdapshtrzmfictsfocornztobrdwtlhgdahxfhlnwhwpchbipmizpeihbdqtasxoetcurohrilkxgiseizpnehtiselcrtaeocophzxeivoagikhjpsbbisexwvstaoconbbisbhczdtaseprosgdihbhzflhpeelocooyhwpigrxgiwipwspvdnokftdphbseomvtxaksgpvbslpdbbhfcvshdihbpydmvtyamigpoydapaligpagriseifxycbdapoyhncagbnlrxtjctasglntzndewwcehxwcoiowsfaeapytasipnmvqzodwhehxqdyceihtogcuehxkwzlxwchhbqwehxftwamwdyshtesiechzpamizphsicytftqigoawywsiprfwcpdtbsehxvpapbbtdshtisevwitzxbhtnmvxdlbtthhbqwsalbdhbxsclsligpdbgrcopbtobrhwpvbgxznhtpyomvtcokobzrxutyekoaoiowhtogwceomkdaakhhxarptldhdipdmvtqikgimohyhtioqdyttwctnzhwpdxgrciihxznhtpdtthtqrtatogxbtcaezntntqrzrwocnepwishxzapnbqcztbccdoyftwizwdyagrbzrtzxeypvxwebbiselsrznwpdzkljmehxvtwlxbxnsmoipilhglnltdcmxrxythoctdxoaviguszmhtesiechzpamdqwawrsaezdehxfvzvxfcxeghhlrxhwppxfkprlwdysmvtdemkdaobbidoyjxpwtftcetzajoidddewocotasdaphgxeihbxdogzngebztobrhwpgxbxfshtewamciseksefbewrwidsiseivppdkihdexwcerhrjntbcceoivppdkihtstbxxpxfupcmkwzlxhwphbuwprewvsthtesiechzpamqcetyhehkcjrhmvtcezialrbhnzfmvtseeztyivhtxpeslsivvpeltgiqawshlwtmxythhwphxokpnlkwptasgehbgxxpxfupcmwdyoygicuvhjcetfxdeltgzmtbtyltfvpmxbizfmvtaltbdcfkcbehxwbaekttntksrznvwapmxbitnmvthrbhtcshkcxigrdqtashernuvwigutwefsceshtisonuwewawrsaksczwywgdtufdfgahizgxhwprumwtmhfepraoedfkcbehxqdxphgxeihbdqtaslzrdoioiyttceghitmxgpcejitdtbccdlbytehxgxxieogbuxgitogoqzumhwpiewpoagrisehrndsxmlsivvpcepcgehtgztnzpjewawrsctbcztaokpawwheigqilnlktcighwpazsdqpeoiztasgpwtgczrxujwakadoehtefbewrltbcclnwoclumvdcwhiaohtjtehxztdslqgfpesxyaehtcigudcawrxygmcphokylsivvllsdbdhnhbajthoupwhtwtsyfxpnwgiseksxdnhoqdukrxeybbhfpichtnzhwltasblyaokpltwssilzpmonfhlsbrtqokoitmxcgeukbtofkcbznxkdckmcpyomvtcagrhfcawceekfjatbccdwhiaobxadceewzplrhdzcvigtnmvtnalsdqaeccrtaoczftgwzrmkgttbbvtntzaltmsbatlhdoemsgxigsisevvgznhzdrivoazrwsgzfmvtalthdyivkgttbbvdogwceekbpweowspnvsisilicnekhptnmmpmonhpyylwcrlxrxllhujpbxwcrchaezsxrpeogsitmxwhldbgifruwcreesbpnmkwtcaajdtuspombhipdmcpqfxqiwogutcwhfzdsnqwlsmvtceiiqwivocotasalwladcemvpysacgeekccpsuiiznmvtztasgsagriselstxigustsvftaagqxpshtiseksefbewrxarccwytfxdehiizfmvtoilqdcdtbiplxatytlkwtcahwppawazshdwpraohltmsbatxrizugwipigohtnzzthhhztaekvpaspwisonhqpiguwtmlsaqauzteoksrzggwhptasxychbhtsmscnypvxnhbgdmvbcjdthihqokhwprxwhljnrvxeghdqayhtcazshhhbqwqepugpamkgttxfhsaostgekptpntpapthoceivweltxtdctasbdeejtdtasnoogciaekqttvxhwpwtbizfvccyeqwdyighwpikclywkwitnzgdctasvlplwcehxwgdylhtxspvxnhtftgilwqwexbdfgahdehhgthhhqdxettiprmvtxighwpbxuxynbbvdoyzxeekoifrxocopawazshdwjafwsehxtxcsmsuqokhhzfmvdfgahpydeocrututxoksxychbhtsmscnixgdncnfisagbdhwascehxdpehlcudpxqjwamwdyakslplekdcntbsehxatlnbbvzfpcgosiftnilsajdxtxyewtdcchbhtsmscnymcdtsmvtrrhkisoyhxxetbsdofsdqtasvcethtdtvftltbccdoyhwphnapymbbssaosqpegkpytbbvtnnbxeymfxpdumisilhtdtlskprtzdqtasewamcctcwwpwozitdavqdcdbbveohigxowsgyiwspdaidtlrmcqpdxttntbjtmumhwpdxtxnixbrjilbdarhcuehthiserktcevcbaolssltwwuqekscetbatdokpnoiyttceghwlnwgpydmvtduidddimwdytaoiehxftauuzxnwtglcimhtyugwceekfjatxrajagrqjavcceigidfsxtuzrmwhtnlcbpdxugpevccqikatobrhwpnnatconggpfxftycxgucofccpptfizfmvthokyizagcisek"
+
+# Este codigo esta otimizado para textos em ingles.
+# Nesta variavel, definimos uma lista com o indice de frequencia das letras no alfabeto ingles (A-Z).
+
+ENGLISH_FREQ = (0.0749, 0.0129, 0.0354, 0.0362, 0.1400, 0.0218, 0.0174, 0.0422, 0.0665, 0.0027, 0.0047,
+                0.0357, 0.0339, 0.0674, 0.0737, 0.0243, 0.0026, 0.0614, 0.0695, 0.0985, 0.0300, 0.0116,
+                0.0169, 0.0028, 0.0164, 0.0004)
+
+# Importacao de modulos utilizadas na solucao
+
+import itertools # Utilizado neste codigo para iterar de forma ciclica em caracteres.
+import string # Utilizado neste codigo para validacao entre maiusculas e minusculas.
+import textwrap # Utilizado neste codigo para formatacao e impressao de texto.
+
+# A funcao 'vigenere_kasiski' trabalha na resolucao de uma cifra de Vigenere encontrando chaves de modo que o texto simples se assemelhe ao ingles normal, utilizando o metodo Kasiski.
+# Esta funcao utiliza como auxiliares as funcoes 'vigenere_compare_freq' e 'vigenere_decrypt'.
+# A funcao esta configurada para apresentar apenas a melhor chave encontrada nos testes de tamanho de chave efetuados, mas podera apresentar mais chaves se necessario ([:1]).
+
+def vigenere_kasiski(text, key_min_size=None, key_max_size=None, a_is_zero=True):
+    best_keys = [] # lista que recebera as melhores chaves encontradas.
+    key_min_size = key_min_size or 1 # Tamanho minimo de chave para busca.
+    key_max_size = key_max_size or 20 # Tamanho maximo de chave para busca.
+
+    text_letters = [c for c in text.lower() if c in string.ascii_lowercase] # Lista com as letras em minusculo do texto criptografado.
+
+    for key_length in range(key_min_size, key_max_size):
+# Testa todos os possiveis tamanhos de chave entre o minimo e maximo configurados.
+# Para cada tamanho de chave, a funcao testa todas as chaves possiveis, calculando o deslocamente para cada letra do texto usando a funcao 'vigenere_compare_freq' e a funcao 'vigenere_decrypt' com todas as letras do alfabeto.
+# A chave com menor valor de 'vigenere_compare_freq' sera a escolhida como a chave mais provavel.
+        key = [None] * key_length
+        for key_index in range(key_length):
+            letters = "".join(itertools.islice(text_letters, key_index, None, key_length))
+            shifts = []
+            for key_char in string.ascii_lowercase:
+                shifts.append(
+                    (vigenere_compare_freq(vigenere_decrypt(letters, key_char, a_is_zero)), key_char)
+                )
+            key[key_index] = min(shifts, key=lambda x: x[0])[1]
+        best_keys.append("".join(key))
+    best_keys.sort(key=lambda key: vigenere_compare_freq(vigenere_decrypt(text, key, a_is_zero)))
+    return best_keys[:1]
+
+# A funcao 'vigenere_logic' implementa a logica da cifra de Vigenere, recebe o texto puro a ser cifrado, a chave que devera ser utilizada na cifragem, alem de um booleano de indexacao.
+# Esta funcao e utilizada como auxiliar no processo de decifragem na funcao 'vigenere_decrypt'.
+# Neste codigo, utilizaremos esta funcao como auxiliar no processo de decifragem, implementado na funcao 'vigenere_decrypt'.
+
+def vigenere_logic(plaintext, key, a_is_zero=True):
+    key = key.lower() # Converte a chave para minuscula
+    key_iter = itertools.cycle(map(ord, key)) # Iterador para repetir a sequencia de valores que corresponderao a cada letra da chave.
+    return "".join(
+        chr(ord('a') + (
+            (next(key_iter) - ord('a') + ord(letter) - ord('a'))
+            + (0 if a_is_zero else 2)
+            ) % 26) if letter in string.ascii_lowercase
+        else letter
+        for letter in plaintext.lower() # Itera sobre cada letra do texto aplicando a funcao de cifragem de Vigenere, baseado na chave informada.
+    )
+
+# A funcao 'vigenere_decrypt' implementa o metodo de cifragem ao contrario, recebendo os mesmos argumentos da funcao 'vigenere_logic' e retornando o texto decifrado.
+
+def vigenere_decrypt(cipher_text, key, a_is_zero=True):
+    key_ind = [ord(k) - ord('a') for k in key.lower()]
+    inverse = "".join(chr(ord('a') +
+            ((26 if a_is_zero else 22) -
+                (ord(k) - ord('a'))
+            ) % 26) for k in key)
+    return vigenere_logic(cipher_text, inverse, a_is_zero)
+
+# A funcao 'vigenere_compare_freq' compara a frequencia das letras do texto cifrado com a frequencia media de ocorrencia das letras no ingles.
+# A funcao executa ao final um calculo de diferenca absoluta entre a frequencia de cada letra do texto cifrado e a frequencia esperada no ingles normal, retornando a soma total dessas diferencas.
+
+def vigenere_compare_freq(text):
+    if not text:
+        return None
+    text = [t for t in text.lower() if t in string.ascii_lowercase]
+    freq = [0] * 26
+    total = float(len(text))
+    for l in text:
+        freq[ord(l) - ord('a')] += 1
+    return sum(abs(f / total - E) for f, E in zip(freq, ENGLISH_FREQ))
+
+# Imprime na tela a(s) melhor(es) chave(s) junto ao texto decifrado.
+
+for key in reversed(vigenere_kasiski(CIPHER_TEXT)):
+    print("Chave encontrada: {!r}".format(key))
+    print("Texto decifrado:")
+    print("=" * 80)
+    print(textwrap.fill(vigenere_decrypt(CIPHER_TEXT, key)))
+    print("=" * 80)
